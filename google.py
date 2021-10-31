6031141596b149e493cc53cd6c54db0e892b0d4a
nothingwithrespecttouniverse <93479274+nothingwithrespecttouniverse@users.noreply.github.com>
2021-10-31 20:06:56 +0530

    I just solved all errors of project

--- google.py
+++ google.py
@@ -1,6 +1,14 @@
 import turtle as t
 
 a=t.Turtle()
+a1.color("green")
+a1.left(180)
+a2=t.Turtle()
+a2.speed(100)
+a2.color("blue")
+a2.left(270)
+a3=t.Turtle()
+a3.speed(100)
 a.speed(100)
 a.color("red")
 a1=t.Turtle()
@@ -24,6 +32,16 @@ for x in range(1,1000):
     a2.forward(x%30)
     a2.left(x%30)
     a3.forward(x%30)
+    a2=t.Turtle()
+a2.speed(100)
+a2.color("blue")
+a2.left(270)
+a3=t.Turtle()
+a3.speed(100)
+a3.color("orange")
+a3.left(90)
+for x in range(1,1000):
+    
     a3.left(x%30)
 
 #for x in range(1,1000):
